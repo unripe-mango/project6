bcb27d18ec33b2badd3471478eaf3f3841c83553
dangtrinhhh <dangtrinhdo2910@gmail.com>
2022-06-12 23:22:50 +0700

    12/6/2022 first commit

--- win32-app/win32-app/resource.h
+++ win32-app/win32-app/resource.h
@@ -21,14 +21,18 @@
 #define IDD_DIALOG8                     136
 #define IDC_BUTTON1                     1005
 #define IDC_BUTTON2                     1006
-#define IDC_EDIT1                       1006
-#define IDC_BUTTON3                     1007
 #define IDC_EDIT2                       1007
 #define IDC_EDIT3                       1008
 #define IDC_EDIT4                       1009
 #define IDC_EDIT5                       1010
+#define IDC_EDIT7                       10101
 #define IDC_EDIT6                       1011
+#define IDC_EDIT8                       10112
+#define IDC_EDIT9                       10122
+#define IDC_EDIT10                      10133
 #define IDD_ABOUTBOX1                   1898
+#define IDC_BUTTON3                     10073
+#define IDC_EDIT1                       10099
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -38,7 +42,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- win32-app/win32-app/win32-app.cpp
+++ win32-app/win32-app/win32-app.cpp
@@ -1,29 +1,65 @@
-// win32-app.cpp : Defines the entry point for the application.
+﻿// win32-app.cpp : Defines the entry point for the application.
 //
 
 #include "framework.h"
 #include "win32-app.h"
-#include "myClass.h"
-#include "Source.cpp"
+#include "my_project.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <cstring>
+#include <atlstr.h>
 
-//#include "source.cpp"
+#ifdef UNICODE
+typedef std::wostringstream tstringstream;
+#else
+typedef std::ostringstream tstringstream;
+#endif
 
+//#include <objidl.h>
+//#include <gdiplus.h>
+//using namespace Gdiplus;
+//#pragma comment (lib,"Gdiplus.lib")
+
+
+//VOID OnPaint(HDC hdc)
+//{
+//    Graphics graphics(hdc);
+//    Pen      pen(Color(255, 0, 0, 255));
+//    graphics.DrawLine(&pen, 100, 100, 500, 500);
+//}
 
 #define MAX_LOADSTRING 100
-TCHAR vl[20], vt[20], vr[20], vb[20];
-int top = 0, left = 0, right = 0, bottom = 0;
-
+TCHAR vl[20], vt[20], vr[20], vb[20], vd[20], ve[20];
+vector<myFigure*> figure;
+myLine line = myLine(0, 0, 0, 0);
+ 
 PAINTSTRUCT ps;
 HBRUSH hbrush;
 HPEN hpen;
+HPEN rpen;
+
+int c1 = 0, c2 = 0, c3 = 0;
+int r1 = 0, r2 = 0, r3 = 0, r4 = 0;
+int e1 = 0, e2 = 0, e3 = 0, e4 = 0;
+int s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0;
+int l1 = 0, l2 = 0, l3 = 0, l4 = 0;
+int t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
+int p1;
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 void addMenu(HWND hwnd);
-INT_PTR CALLBACK Input(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
-INT_PTR CALLBACK Input1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+INT_PTR CALLBACK Input_R(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Input_C(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Input_E(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Input_S(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Input_P(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Input_T(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK Input_L(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -126,7 +162,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
 //  PURPOSE: Processes messages for the main window.
@@ -138,67 +173,52 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    /*static Ellipse e(500, 300, 200, 100);
+    static Circle c(500, 300, 200);
+    static Point t(400, 300);
+    WCHAR msg[][50] = { L"Outside",L"Inside" };*/
     switch (message)
     {
     case WM_CREATE:
         addMenu(hWnd);
     case WM_COMMAND:
         {
-            hpen = CreatePen(PS_SOLID, 4, RGB(0, 0, 255));
-            HDC hdc = BeginPaint(hWnd, &ps);
             int wmId = LOWORD(wParam);
             // Parse the menu selections:
             switch (wmId)
             {
-            //Square - Rectangle - Circle - Semi-Circle - Ellipse - Polygon - Triangle - Line 
+            ////Square - Rectangle - Circle - Semi-Circle - Ellipse - Polygon - Triangle - Line 
             case 551:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX1), hWnd, Input);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX1), hWnd, Input_R);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Rectangle(hdc, left, top, right, bottom);
-                break;
+            break; 
             case 552:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, Input);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, Input_R);
                 InvalidateRect(hWnd, NULL, TRUE);
-                hpen = CreatePen(PS_SOLID, 2, RGB(140, 0, 0));
-                SelectObject(hdc, hpen); 
-                Ellipse(hdc, left, top, right, bottom);
                 break;
             case 553:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hWnd, Input1);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hWnd, Input_C);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Ellipse(hdc, left, top, top, bottom);
                 break;
             case 554:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG3), hWnd, Input1);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG3), hWnd, Input_S);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Ellipse(hdc, left, top, top, bottom);
                 break;
             case 555:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG5), hWnd, Input1);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG5), hWnd, Input_E);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Ellipse(hdc, left, top, top, bottom);
                 break; 
             case 556:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG6), hWnd, Input1);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG6), hWnd, Input_P);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Ellipse(hdc, left, top, top, bottom);
-                break;
+                break; 
             case 557:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG7), hWnd, Input1);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG7), hWnd, Input_T);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Ellipse(hdc, left, top, top, bottom);
-                break;
+            break; 
             case 558:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG8), hWnd, Input1);
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG8), hWnd, Input_L);
                 InvalidateRect(hWnd, NULL, TRUE);
-                SelectObject(hdc, hpen);
-                Ellipse(hdc, left, top, top, bottom);
                 break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
@@ -212,30 +232,45 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_PAINT:
-        {
-            hpen = CreatePen(PS_SOLID, 4, RGB(0, 0, 255));
+        {         
+            PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            int wmId = LOWORD(wParam);
-            //draw a rectangle with color blue               
-            SelectObject(hdc, hpen);
-            Rectangle(hdc, right, top, left, bottom);
-            //break;
-            //draw a ellipse
-            /*hpen = CreatePen(PS_SOLID, 2, RGB(140, 0, 0));
-            SelectObject(hdc, hpen);
-            Ellipse(hdc, left, top, right, bottom);*/
-            //break;
-            //Ellipse(hdc, 500, 100, 100, 500);
-            //draw a circle
-            hpen = CreatePen(PS_SOLID, 2, RGB(140, 0, 0));
-            SelectObject(hdc, hpen);
-            //Ellipse(hdc, left - right, top + right, right + left, top - right);
-            break;
-            
-                    
-            DeleteObject(hpen);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
+            hpen = CreatePen(PS_SOLID, 4, RGB(0, 0, 255));
+            rpen = CreatePen(PS_SOLID, 4, RGB(255, 0, 0));
+
+            int count = 0;
+
+            SelectObject(hdc, GetStockObject(NULL_BRUSH));
+
+            for (int i = 0; i < figure.size(); i++) {
+                SelectObject(hdc, hpen);
+                figure[i]->Draw(hdc);
+            }
+
+            if (line.e.getX() != 0 || line.s.getY() != 0 || line.e.getX() != 0|| line.e.getY() != 0) {
+                for (int i = 0; i < figure.size(); i++) {
+                    if (figure[i]->isCutByLine(line) == true) {
+                        SelectObject(hdc, rpen);
+                        figure[i]->Draw(hdc);
+                        count++;
+                    }
+                }
+            }
+                               
+            line.Draw(hdc);
+
+            FILE* stream;
+            freopen_s(&stream, "check.txt", "w", stdout);
+            cout << "The number of shapes is cut by line: " << count;
+
+            SetTextColor(hdc, RGB(0, 100, 0));
+
+            tstringstream stros;
+            stros << "The number of shapes is cut by line : " << count;
+            //::MessageBox(hWnd, stros.str().c_str(), L"", MB_OK);
+            TextOut(hdc, 1100, 20, stros.str().c_str(), 40);
+            count = 0;
+            EndPaint(hWnd, &ps);          
         }
         break;
     case WM_DESTROY:
@@ -267,10 +302,8 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     return (INT_PTR)FALSE;
 }
 
-INT_PTR CALLBACK Input(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK Input_R(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    HDC hdc;
-    PAINTSTRUCT ps;
     UNREFERENCED_PARAMETER(lParam);
     switch (message)
     {
@@ -281,18 +314,16 @@ INT_PTR CALLBACK Input(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
             GetDlgItemText(hDlg, IDC_EDIT1, vl, sizeof(vl));
-            left = _ttoi(vl);
+            r1 = _ttoi(vl);
             GetDlgItemText(hDlg, IDC_EDIT2, vt, sizeof(vt));
-            top = _ttoi(vt);
+            r2 = _ttoi(vt);
             GetDlgItemText(hDlg, IDC_EDIT3, vr, sizeof(vr));
-            right = _ttoi(vr);
+            r3 = _ttoi(vr);
             GetDlgItemText(hDlg, IDC_EDIT4, vb, sizeof(vb));
-            bottom = _ttoi(vb);
+            r4 = _ttoi(vb);
 
-            /*OutputDebugString(vl);
-            OutputDebugString(vt);
-            OutputDebugString(vr);
-            OutputDebugString(vb);*/
+            myFigure* f = new myRectangle(r3, r4, r1, r2);
+            figure.push_back(f);
 
             EndDialog(hDlg, LOWORD(wParam));
             return (INT_PTR)TRUE;
@@ -302,10 +333,8 @@ INT_PTR CALLBACK Input(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     return (INT_PTR)FALSE;
 }
 
-INT_PTR CALLBACK Input1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK Input_C(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    HDC hdc;
-    PAINTSTRUCT ps;
     UNREFERENCED_PARAMETER(lParam);
     switch (message)
     {
@@ -316,12 +345,206 @@ INT_PTR CALLBACK Input1(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
             GetDlgItemText(hDlg, IDC_EDIT1, vl, sizeof(vl));
-            left = _ttoi(vl);
+            c1 = _ttoi(vl);
+            //OutputDebugString(vl);
             GetDlgItemText(hDlg, IDC_EDIT2, vt, sizeof(vt));
-            top = _ttoi(vt);
+            c2 = _ttoi(vt);
             GetDlgItemText(hDlg, IDC_EDIT3, vr, sizeof(vr));
-            right = _ttoi(vr);
+            c3 = _ttoi(vr);
+            myFigure* f = new myCircle(c1, c2, c3);
+            figure.push_back(f);
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
 
+INT_PTR CALLBACK Input_E(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            GetDlgItemText(hDlg, IDC_EDIT1, vl, sizeof(vl));
+            e1 = _ttoi(vl);
+            //OutputDebugString(vl);
+            GetDlgItemText(hDlg, IDC_EDIT2, vt, sizeof(vt));
+            e2 = _ttoi(vt);
+            GetDlgItemText(hDlg, IDC_EDIT3, vr, sizeof(vr));
+            e3 = _ttoi(vr);
+            GetDlgItemText(hDlg, IDC_EDIT4, vb, sizeof(vb));
+            e4 = _ttoi(vb);
+            myFigure* f = new myEllipse(e1, e2, e3, e4);
+            figure.push_back(f);
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Input_S(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            GetDlgItemText(hDlg, IDC_EDIT1, vl, sizeof(vl));
+            s1 = _ttoi(vl);
+            //OutputDebugString(vl);
+            GetDlgItemText(hDlg, IDC_EDIT2, vt, sizeof(vt));
+            s2 = _ttoi(vt);
+            GetDlgItemText(hDlg, IDC_EDIT3, vr, sizeof(vr));
+            s3 = _ttoi(vr);
+            GetDlgItemText(hDlg, IDC_EDIT4, vb, sizeof(vb));
+            s4 = _ttoi(vb);
+            GetDlgItemText(hDlg, IDC_EDIT5, vd, sizeof(vd));
+            s5 = _ttoi(vd);
+            myFigure* f = new mySemiCircle(s1, s2, s3, s4, s5);
+
+            figure.push_back(f);
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+vector <int> extractIntegerWords(string str)
+{
+    stringstream ss;
+    ss << str;
+    std::vector<int> a;
+    string temp;
+    int found;
+    while (!ss.eof()) {
+        ss >> temp;
+        if (stringstream(temp) >> found)
+            a.push_back(found);
+        temp = "";
+    }
+    return a;
+}
+
+INT_PTR CALLBACK Input_P(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            vector<int> p2;
+            GetDlgItemText(hDlg, IDC_EDIT3, vl, sizeof(vl));
+            
+            wstring t(&vl[0]);
+            string s(t.begin(), t.end());
+            p2 = extractIntegerWords(s);
+
+            vector<myPoint> mp;
+            for (int i = 0; i < p2.size(); i += 2) {
+                myPoint p = myPoint(p2[i], p2[i + 1]);
+                mp.push_back(p);
+                cout << p.getX() << " " << p.getY();           
+            }
+            myFigure* f = new myPolygon(mp);
+            figure.push_back(f);
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Input_T(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            GetDlgItemText(hDlg, IDC_EDIT1, vl, sizeof(vl));
+            t1 = _ttoi(vl);
+            //OutputDebugString(vl);
+            GetDlgItemText(hDlg, IDC_EDIT2, vt, sizeof(vt));
+            t2 = _ttoi(vt);
+            GetDlgItemText(hDlg, IDC_EDIT3, vr, sizeof(vr));
+            t3 = _ttoi(vr);
+            GetDlgItemText(hDlg, IDC_EDIT4, vb, sizeof(vb));
+            t4 = _ttoi(vb);
+            GetDlgItemText(hDlg, IDC_EDIT5, vd, sizeof(vd));
+            t5 = _ttoi(vd);
+            GetDlgItemText(hDlg, IDC_EDIT6, ve, sizeof(ve));
+            t6 = _ttoi(ve);
+
+            OutputDebugString(vl);
+            OutputDebugString(vt);
+            OutputDebugString(vr);
+            OutputDebugString(vb);
+            OutputDebugString(vd);
+            OutputDebugString(ve);
+
+            myFigure* f = new myTriangle(t1, t2, t3, t4, t5, t6);
+            figure.push_back(f);
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Input_L(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            GetDlgItemText(hDlg, IDC_EDIT7, vl, sizeof(vl));
+            l1 = _ttoi(vl);
+            //OutputDebugString(vl);
+            GetDlgItemText(hDlg, IDC_EDIT8, vt, sizeof(vt));
+            l2 = _ttoi(vt);
+            GetDlgItemText(hDlg, IDC_EDIT9, vr, sizeof(vr));
+            l3 = _ttoi(vr);
+            GetDlgItemText(hDlg, IDC_EDIT10, vb, sizeof(vb));
+            l4 = _ttoi(vb);
+
+            OutputDebugString(vl);
+            OutputDebugString(vt);
+            OutputDebugString(vr);
+            OutputDebugString(vb);
+
+            line = myLine(l1, l2, l3, l4);
+            //line.push_back(l);
+            //figure.push_back(f);
             EndDialog(hDlg, LOWORD(wParam));
             return (INT_PTR)TRUE;
         }
@@ -343,13 +566,15 @@ void addMenu(HWND hwnd) {
     AppendMenu(hMenu, MF_STRING, 556, L"Polygon");
     AppendMenu(hMenu, MF_STRING, 557, L"Triangle");
     AppendMenu(hMenu, MF_STRING, 558, L"Line");
+
     SetMenu(hwnd, hMenu);
 }
 
-void Circle::Draw(HDC hdc)
-{
-    //hpen = CreatePen(PS_SOLID, 2, RGB(140, 0, 0));
-    //SelectObject(hdc, hpen);
-    int x = O.getX(), y = O.getY();
-    Ellipse(hdc, x - R, y + R, R + x, y - R);
-}
+// Square: 100 100 250 250
+// Rectangle: 100 350 500 450
+// Circle: 800 500 150
+// Semi-Circle: 1200 600 150 150 1
+// Ellipse: 1000 200 200 100
+// Polygon: 150 450 250 520 200 620 110 620 65 520
+// Triangle: 500 50 650 300 350 300
+// Line: 0 0 1400 650
